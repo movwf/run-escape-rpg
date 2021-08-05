0996340730f8ceb13bd64d883f50f1c8e7c9f6f1
movwf <movwf@protonmail.com>
2021-08-05 18:42:52 +0300

    Conf: Character model (avatar, sprite) data created.

---
+++ src/config/CharacterModels.js
@@ -0,0 +1,27 @@
+/*
+  Character settings
+*/
+
+// eslint-disable-next-line import/prefer-default-export
+export const Characters = {
+  'char-1': {
+    avatar: 'https://i.ibb.co/7zwK9vG/char-1.png',
+    sprite: 'https://i.ibb.co/ry9mJ9f/char-1.png',
+  },
+  'char-2': {
+    avatar: 'https://i.ibb.co/nzhwKL5/char-2.png',
+    sprite: 'https://i.ibb.co/3Cm0Lkr/char-2.png  ',
+  },
+  'char-3': {
+    avatar: 'https://i.ibb.co/9TdGJmQ/char-3.png',
+    sprite: 'https://i.ibb.co/sPLD0fV/char-3.png  ',
+  },
+  'char-4': {
+    avatar: 'https://i.ibb.co/kGKShpf/char-4.png',
+    sprite: 'https://i.ibb.co/MZKxwk5/char-4.png',
+  },
+  'char-5': {
+    avatar: 'https://i.ibb.co/Tbk2twZ/char-5.png',
+    sprite: 'https://i.ibb.co/27MPvwQ/char-5.png',
+  },
+};
