3017e67cfd99a23d33b7dc149630e697941eeb30
movwf <movwf@protonmail.com>
2021-08-05 19:24:45 +0300

    Conf: Keyboard shortcut settings initiated.

---
+++ src/config/KeyboardSettings.js
@@ -0,0 +1,13 @@
+/*
+  Keyboard shortcut keycodes
+*/
+const KeyboardSettings = {
+  SPACE_KEY: 32,
+  ESCAPE_KEY: 27,
+  ARROW_UP_KEY: 38,
+  ARROW_DOWN_KEY: 40,
+  ARROW_LEFT_KEY: 37,
+  ARROW_RIGHT_KEY: 39,
+};
+
+export default KeyboardSettings;
