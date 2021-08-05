ec07b80ca13cf57d8e3aa577d64a7e2f09fadbca
movwf <movwf@protonmail.com>
2021-08-05 18:22:33 +0300

    UI: Map component created.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import './App.css';
+import Map from './components/Map';
 import Logo from './components/Logo';
 import Frame from './components/Frame';
 import Footer from './components/Footer';
 
 function App() {
+  const mapRef = React.useRef();
+
   return (
     <div className="App">
       <Logo />
       <div className="Game">
-        <Frame>Map</Frame>
+        <Frame>
+          <Map ref={mapRef} mapImage="https://i.ibb.co/gJcWXnZ/boardgame.png">
+            Character
+          </Map>
+        </Frame>
       </div>
       <Footer />
     </div>
