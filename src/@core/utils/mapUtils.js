a227c8e278fe5dfc06154a43c9899add79e2ae82
movwf <movwf@protonmail.com>
2021-08-05 18:56:36 +0300

    Logic-Util: Gamemap starting position calculation function created.

--- src/@core/utils/mapUtils.js
+++ src/@core/utils/mapUtils.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-labels */
+/* eslint-disable no-restricted-syntax */
 import explodeArray from '../../utils/explodeArray';
 
 /**
@@ -6,7 +8,6 @@ import explodeArray from '../../utils/explodeArray';
  * @param {String} pathLayerName Map layer contains path
  * @returns Matrix of map and it's properties
  */
-// eslint-disable-next-line import/prefer-default-export
 export const createMapData = (mapObject, pathLayerName) => {
   const { layers, tileheight, tilewidth } = mapObject;
   // Select path layer
@@ -18,5 +19,30 @@ export const createMapData = (mapObject, pathLayerName) => {
     width: pathLayer.width,
     tileHeight: tileheight,
     tileWidth: tilewidth,
+  };
+};
+
+/**
+ * Calculates starting position of the player based on path layer of map
+ * @param {Object} pathLayerData Path layer object
+ * @returns Start position of player
+ */
+export const getStartPosition = (pathLayerData) => {
+  const { map, height, width, tileHeight, tileWidth } = pathLayerData;
+  let firstAvailableTile = { x: 0, y: 0 };
+
+  // Look for first movable tile location
+  search: for (let i = 0; i < height; i += 1) {
+    for (let k = 0; k < width; k += 1) {
+      if (map[i][k] === 0) {
+        firstAvailableTile = { x: i, y: k };
+        break search;
+      }
+    }
+  }
+
+  return {
+    x: firstAvailableTile.x * tileHeight,
+    y: firstAvailableTile.y * tileWidth,
   };
 };
